4986a5cb5973184c80c147d7112002d4c3799c85
Shawn Stricker <kb1ibt@me.com>
2011-08-26 15:47:43 -0400

    Initial Commit of the Login form files

---
+++ FetLife/FLLoginViewController.h
@@ -0,0 +1,18 @@
+//
+//  FLLoginController.h
+//  FetLife
+//
+//  Created by Shawn Stricker on 8/23/11.
+//  Copyright (c) 2011 KB1IBT.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLLoginViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *username;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+
+- (IBAction)login:(id)sender
+
+@end
---
+++ FetLife/FLLoginViewController.m
@@ -0,0 +1,51 @@
+//
+//  FLLoginController.m
+//  FetLife
+//
+//  Created by Shawn Stricker on 8/23/11.
+//  Copyright (c) 2011 KB1IBT.com. All rights reserved.
+//
+
+#import "FLLoginViewController.h"
+
+@implementation FLLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	return YES;
+}
+
+@end
