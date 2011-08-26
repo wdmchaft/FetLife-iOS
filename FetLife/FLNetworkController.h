ffa5129ba7c8267a4ffe9f757e010d50a01766d5
Shawn Stricker <kb1ibt@me.com>
2011-08-26 15:50:31 -0400

    Initial commit of the Network controls

---
+++ FetLife/FLNetworkController.h
@@ -0,0 +1,13 @@
+//
+//  FLNetworkController.h
+//  FetLife
+//
+//  Created by Shawn Stricker on 8/26/11.
+//  Copyright (c) 2011 KB1IBT.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLNetworkController : NSObject
+
+@end
---
+++ FetLife/FLNetworkController.m
@@ -0,0 +1,13 @@
+//
+//  FLNetworkController.m
+//  FetLife
+//
+//  Created by Shawn Stricker on 8/26/11.
+//  Copyright (c) 2011 KB1IBT.com. All rights reserved.
+//
+
+#import "FLNetworkController.h"
+
+@implementation FLNetworkController
+
+@end
