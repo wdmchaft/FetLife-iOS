8988388cdc2a9b18fd587c878fc07f03ac9f4701
Shawn Stricker <kb1ibt@me.com>
2011-09-08 00:44:55 -0400

    The login script seems to be working (now to watch out for a dreaded captcha) Also the login check seems to be working

--- FetLife/FLNetworkController.m
+++ FetLife/FLNetworkController.m
@@ -7,11 +7,15 @@
 //
 
 #import "FLNetworkController.h"
-#import "SBJson.h"
+#import <SBJson/SBJson.h>
+
+@class SBJsonParser;
+@class SBJsonWriter;
 
 @implementation FLNetworkController
 
 +(BOOL) loggedIn {
+    SBJsonParser *_parser = [[SBJsonParser alloc] init];
     NSError *theError = nil;
     UIWebView *webView = [[UIWebView alloc] init];
     NSString *ua = [webView stringByEvaluatingJavaScriptFromString:@"navigator.userAgent"];
@@ -20,18 +24,26 @@
     [request setHTTPMethod:@"GET"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
     [request setValue: ua forHTTPHeaderField: @"User-Agent"];
-    
-    return NO;
+
+    NSHTTPURLResponse *theResponse =[[NSHTTPURLResponse alloc]init];
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&theResponse error:&theError];
+    if (theResponse.statusCode == 500) {
+        return NO;
+    }
+    return YES;
 }
 
 +(BOOL) loginWithUsername:(NSString *)username withPassword:(NSString *)password {
+    SBJsonParser *_parser = [[SBJsonParser alloc] init];
+    SBJsonWriter *_writer = [[SBJsonWriter alloc] init];
+    _writer.sortKeys = YES;
     UIWebView *webView = [[UIWebView alloc] init];
     NSString *ua = [webView stringByEvaluatingJavaScriptFromString:@"navigator.userAgent"];
     NSError *theError = nil;
     NSArray *keys = [NSArray arrayWithObjects:@"nickname_or_email", @"password", @"commit", nil];
     NSArray *objects = [NSArray arrayWithObjects:username, password, @"Login to FetLife", nil];
     NSDictionary *jsonDictionary = [NSDictionary dictionaryWithObjects:objects forKeys:keys];
-    NSString *jsonString = [jsonDictionary JSONRepresentation];
+    NSString *jsonString = [_writer stringWithObject:jsonDictionary];
     NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
 
     
@@ -45,9 +57,12 @@
     [request setHTTPBody:jsonData];
 
     
-    NSURLResponse *theResponse =[[NSURLResponse alloc]init];
+    NSHTTPURLResponse *theResponse =[[NSHTTPURLResponse alloc]init];
     NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&theResponse error:&theError];
-    
+
+    if (theResponse.statusCode!=200) {
+        return NO;
+    }
     return YES;
 }
 
